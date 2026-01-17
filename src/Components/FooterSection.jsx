d2c13209ad2e97062da0ad4215b09281756d8dfb
insharahkalam <insharahkalam19@gmail.com>
2026-01-17 11:24:35 +0500

    updated

--- src/Components/FooterSection.jsx
+++ src/Components/FooterSection.jsx
@@ -7,8 +7,9 @@ const FooterSection = () => {
                     {/* Column 1 */}
                     <div className="col-md-3 mb-4">
                         <h5 className="fw-bold">Meta Open Source</h5>
-                        <p className="text-secondary mt-3">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        <p className="text-white textSmall mt-3">
+                            Copyright © Meta Platforms, Inc <br />
+                            uwu?
                         </p>
                     </div>
 
@@ -16,12 +17,12 @@ const FooterSection = () => {
                     <div className="col-md-2 mb-4">
                         <h6 className="fw-bolder fs-5">Learn React</h6>
                         <ul className="list-unstyled mt-4 text-white">
-                            <li className="my-2 fw-normal textSmall ">Quick Start</li>
-                            <li className="my-2 fw-normal textSmall ">Installation</li>
-                            <li className="my-2 fw-normal textSmall ">Describing the UI</li>
-                            <li className="my-2 fw-normal textSmall ">Adding Interactivity</li>
-                            <li className="my-2 fw-normal textSmall ">Managing State</li>
-                            <li className="my-2 fw-normal textSmall ">Escape Hatches</li>
+                            <li className="my-2 textSmall ">Quick Start</li>
+                            <li className="my-2 textSmall ">Installation</li>
+                            <li className="my-2 textSmall ">Describing the UI</li>
+                            <li className="my-2 textSmall ">Adding Interactivity</li>
+                            <li className="my-2 textSmall ">Managing State</li>
+                            <li className="my-2 textSmall ">Escape Hatches</li>
                         </ul>
                     </div>
 
@@ -29,8 +30,8 @@ const FooterSection = () => {
                     <div className="col-md-2 mb-4">
                         <h6 className="fw-bolder fs-5">API Reference</h6>
                         <ul className="list-unstyled mt-4 text-white">
-                            <li className="my-2 fw-normal textSmall">React APIs</li>
-                            <li className="my-2 fw-normal textSmall">React DOM</li>
+                            <li className="my-2 textSmall">React APIs</li>
+                            <li className="my-2 textSmall">React DOM</li>
                         </ul>
                     </div>
 
@@ -38,10 +39,10 @@ const FooterSection = () => {
                     <div className="col-md-2 mb-4">
                         <h6 className="fw-bolder fs-5">Community</h6>
                         <ul className="list-unstyled mt-4 text-white">
-                            <li className="my-2 fw-normal textSmall">Code of Conduct</li>
-                            <li className="my-2 fw-normal textSmall">Meet the Team</li>
-                            <li className="my-2 fw-normal textSmall">Docs Contributors</li>
-                            <li className="my-2 fw-normal textSmall">Acknowledgements</li>
+                            <li className="my-2 textSmall">Code of Conduct</li>
+                            <li className="my-2 textSmall">Meet the Team</li>
+                            <li className="my-2 textSmall">Docs Contributors</li>
+                            <li className="my-2 textSmall">Acknowledgements</li>
                         </ul>
                     </div>
 
@@ -49,10 +50,10 @@ const FooterSection = () => {
                     <div className="col-md-2 mb-4">
                         <h6 className="fw-bolder fs-5">More</h6>
                         <ul className="list-unstyled mt-3 text-white">
-                            <li className="my-2 fw-normal textSmall">Blog</li>
-                            <li className="my-2 fw-normal textSmall">React Native</li>
-                            <li className="my-2 fw-normal textSmall">Privacy</li>
-                            <li className="my-2 fw-normal textSmall">Terms</li>
+                            <li className="my-2 textSmall">Blog</li>
+                            <li className="my-2 textSmall">React Native</li>
+                            <li className="my-2 textSmall">Privacy</li>
+                            <li className="my-2 textSmall">Terms</li>
 
                         </ul>
                     </div>
